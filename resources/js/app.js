cfce32044f2d0ff52611ce9502b63185a49b6a6f
Nurul Anam <nurulanam.bd@gmail.com>
2023-09-15 21:03:58 +0600

    feat: add owl for categories fix: user darkMode

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,5 @@
 import './bootstrap';
 import 'flowbite';
+
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,20 +26,20 @@
                             </path>
                         </svg>
                     </button>
-                    <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
+                    <a href="#" class="flex ml-2 md:mr-24">
+                        <img src="{{ asset('assets/img/logo/logo.png') }}" class="h-8 mr-3" alt="FlowBite Logo" />
                         <span
-                            class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                            class="self-center text-xl font-bold whitespace-nowrap text-orange-400 dark:text-white">Sunset Sales</span>
                     </a>
                 </div>
                 <div class="flex items-center">
                     <div class="flex items-center ml-3">
                         <div class="flex space-x-4">
                             <button class="dark-toggle dark:text-orange-100 ">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 384 512"><path d="M144.7 98.7c-21 34.1-33.1 74.3-33.1 117.3c0 98 62.8 181.4 150.4 211.7c-12.4 2.8-25.3 4.3-38.6 4.3C126.6 432 48 353.3 48 256c0-68.9 39.4-128.4 96.8-157.3zm62.1-66C91.1 41.2 0 137.9 0 256C0 379.7 100 480 223.5 480c47.8 0 92-15 128.4-40.6c1.9-1.3 3.7-2.7 5.5-4c4.8-3.6 9.4-7.4 13.9-11.4c2.7-2.4 5.3-4.8 7.9-7.3c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-3.7 .6-7.4 1.2-11.1 1.6c-5 .5-10.1 .9-15.3 1c-1.2 0-2.5 0-3.7 0c-.1 0-.2 0-.3 0c-96.8-.2-175.2-78.9-175.2-176c0-54.8 24.9-103.7 64.1-136c1-.9 2.1-1.7 3.2-2.6c4-3.2 8.2-6.2 12.5-9c3.1-2 6.3-4 9.6-5.8c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-3.6-.3-7.1-.5-10.7-.6c-2.7-.1-5.5-.1-8.2-.1c-3.3 0-6.5 .1-9.8 .2c-2.3 .1-4.6 .2-6.9 .4z"/></svg>
                             </button>
                             <button type="button"
-                                class="flex text-sm bg-red-200 rounded-full focus:ring-4 focus:ring-red-300 dark:focus:ring-red-600"
+                                class="flex text-sm bg-red-200 rounded-full focus:ring-4 focus:ring-red-300"
                                 aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                 <span class="sr-only">Open user menu</span>
                                 <img class="w-8 h-8 rounded-full"
@@ -50,7 +50,7 @@
                         <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
                             id="dropdown-user">
                             <div class="px-4 py-3" role="none">
-                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                <p class="text-sm font-semibold text-gray-900 dark:text-white" role="none">
                                     Neil Sims
                                 </p>
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
@@ -61,17 +61,7 @@
                                 <li>
                                     <a href="#"
                                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Dashboard</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Settings</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Earnings</a>
+                                        role="menuitem">Profile</a>
                                 </li>
                                 <li>
                                     <a href="#"
@@ -91,57 +81,57 @@
         aria-label="Sidebar">
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="grid grid-cols-12 gap-3">
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/dashboardIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Dashboard</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/shopping-bagIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Orders</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/branchesIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Branches</span>
                     </a>
                 </li>
 
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/productIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Products</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/discountIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Discounts</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/invoiceIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Invoices</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/userIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Users</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/expencesIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Expences</span>
                     </a>
                 </li>
-                <li class="col-span-6 transition-all ease-linear hover:bg-orange-100 rounded-lg py-5 px-2 group">
-                    <a href="#" class="flex flex-col justify-center items-center">
+                <li class="sidebar-list-item group">
+                    <a href="#" class="sidebar-list-link">
                         <img src="{{ asset('/assets/img/settingIcon.png') }}" alt="">
                         <span class="dark:text-orange-100 dark:group-hover:text-black mt-2">Settings</span>
                     </a>
@@ -150,10 +140,10 @@
         </div>
     </aside>
 
-    <div class="p-4 sm:ml-64">
-        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-            <div class="grid grid-cols-12 gap-4 min-h-[85vh]">
-                <div class="col-span-12 lg:col-span-8 bg-orange-50 min-h-full rounded-lg border p-4">
+    <div class="p-4 sm:ml-64 dark:bg-slate-700">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-orange-100 mt-14">
+            <div class="grid grid-cols-12 gap-4 min-h-[87vh]">
+                <div class="col-span-12 lg:col-span-8 bg-orange-50 dark:bg-gray-800 min-h-full rounded-lg border dark:border-gray-500 p-4">
                     <form id="topSearch">
                         <label for="default-search"
                             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
@@ -170,8 +160,33 @@
                                 </svg><span class="hidden md:block">Search</span></button>
                         </div>
                     </form>
+                    <div class="categories">
+                        <div class="owl-carousel owl-theme my-2">
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Dairy product</p>
+                            </div>
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Vegetables</p>
+                            </div>
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Fruits</p>
+                            </div>
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Meat</p>
+                            </div>
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Grain</p>
+                            </div>
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Fish</p>
+                            </div>
+                            <div class="category-item" data-categroy="name">
+                                <p class="text-gray-800">Desserts</p>
+                            </div>
+                          </div>
+                    </div>
                 </div>
-                <div class="col-span-12 lg:col-span-4 bg-orange-50 min-h-full rounded-lg border p-4">
+                <div class="col-span-12 lg:col-span-4 bg-orange-100 dark:bg-gray-800 min-h-full rounded-lg border dark:border-gray-500 p-4">
 
                 </div>
             </div>
@@ -181,6 +196,29 @@
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script>
         $(document).ready(function() {
+            $('.owl-carousel').owlCarousel({
+                margin: 8,
+                loop: true,
+                autoplay: true,
+                autoplayHoverPause: true,
+                dots : false,
+                responsive : {
+                    0 : {
+                        items:1,
+                    },
+                    640 : {
+                        items:2,
+                    },
+                    768 : {
+                        items:3,
+                    },
+                    1024 : {
+                        items:4,
+                    }
+                }
+            });
+
+
             var isDarkModeEnabled = localStorage.getItem('darkMode');
 
             if (isDarkModeEnabled === 'true') {
@@ -192,11 +230,10 @@
                 if ($('body').hasClass('dark')) {
                     $('body').removeClass('dark');
                     localStorage.setItem('darkMode', 'false');
-                    $('.dark-toggle').html('<svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 384 512"><path d="M223.5 32C100 32 0 132.3 0 256S100 480 223.5 480c60.6 0 115.5-24.2 155.8-63.4c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6c-96.9 0-175.5-78.8-175.5-176c0-65.8 36-123.1 89.3-153.3c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"/></svg>')
+                    $('.dark-toggle').html('<svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 384 512"><path d="M144.7 98.7c-21 34.1-33.1 74.3-33.1 117.3c0 98 62.8 181.4 150.4 211.7c-12.4 2.8-25.3 4.3-38.6 4.3C126.6 432 48 353.3 48 256c0-68.9 39.4-128.4 96.8-157.3zm62.1-66C91.1 41.2 0 137.9 0 256C0 379.7 100 480 223.5 480c47.8 0 92-15 128.4-40.6c1.9-1.3 3.7-2.7 5.5-4c4.8-3.6 9.4-7.4 13.9-11.4c2.7-2.4 5.3-4.8 7.9-7.3c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-3.7 .6-7.4 1.2-11.1 1.6c-5 .5-10.1 .9-15.3 1c-1.2 0-2.5 0-3.7 0c-.1 0-.2 0-.3 0c-96.8-.2-175.2-78.9-175.2-176c0-54.8 24.9-103.7 64.1-136c1-.9 2.1-1.7 3.2-2.6c4-3.2 8.2-6.2 12.5-9c3.1-2 6.3-4 9.6-5.8c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-3.6-.3-7.1-.5-10.7-.6c-2.7-.1-5.5-.1-8.2-.1c-3.3 0-6.5 .1-9.8 .2c-2.3 .1-4.6 .2-6.9 .4z"/></svg>')
                 } else {
                     $('body').addClass('dark');
                 $('.dark-toggle').html('<svg xmlns="http://www.w3.org/2000/svg" height="1.3em" viewBox="0 0 384 512" fill="#fff"><path d="M297.2 248.9C311.6 228.3 320 203.2 320 176c0-70.7-57.3-128-128-128S64 105.3 64 176c0 27.2 8.4 52.3 22.8 72.9c3.7 5.3 8.1 11.3 12.8 17.7l0 0c12.9 17.7 28.3 38.9 39.8 59.8c10.4 19 15.7 38.8 18.3 57.5H109c-2.2-12-5.9-23.7-11.8-34.5c-9.9-18-22.2-34.9-34.5-51.8l0 0 0 0c-5.2-7.1-10.4-14.2-15.4-21.4C27.6 247.9 16 213.3 16 176C16 78.8 94.8 0 192 0s176 78.8 176 176c0 37.3-11.6 71.9-31.4 100.3c-5 7.2-10.2 14.3-15.4 21.4l0 0 0 0c-12.3 16.8-24.6 33.7-34.5 51.8c-5.9 10.8-9.6 22.5-11.8 34.5H226.4c2.6-18.7 7.9-38.6 18.3-57.5c11.5-20.9 26.9-42.1 39.8-59.8l0 0 0 0 0 0c4.7-6.4 9-12.4 12.7-17.7zM192 128c-26.5 0-48 21.5-48 48c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-44.2 35.8-80 80-80c8.8 0 16 7.2 16 16s-7.2 16-16 16zm0 384c-44.2 0-80-35.8-80-80V416H272v16c0 44.2-35.8 80-80 80z"/></svg>');
-
                     localStorage.setItem('darkMode', 'true');
                 }
             });
